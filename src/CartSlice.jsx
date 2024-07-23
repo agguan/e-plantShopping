976bd46614358bff0282c9db0c4f40cd62d8d617
agguan <andrewg1153@gmail.com>
2024-07-23 10:11:49 -0400

    task 3 updates

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,6 +4,7 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0, //initial totalQuantity
   },
   reducers: {
     addItem: (state, action) => {
@@ -11,17 +12,23 @@ export const CartSlice = createSlice({
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
             existingItem.quantity++;
+            state.totalQuantity++; //update totalQuantity
         } else {
             state.items.push({ name, image, cost, quantity: 1 });
+            state.totalQuantity++; //update totalQuantity
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
-    },
+        const itemToRemove = state.items.find(item => item.name === action.payload);
+        if (itemToRemove) {
+            state.totalQuantity -= itemToRemove.quantity; // Decrement totalQuantity
+            state.items = state.items.filter(item => item.name !== action.payload);
+        }    },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
+            state.totalQuantity += quantity - itemToUpdate.quantity; //update totalQuantity
             itemToUpdate.quantity = quantity;
         }
     
